31636e7c23e1a8e2eb758c7916e0085cbb1963a3
bl00m <leon.pierre@openmailbox.org>
2017-05-30 14:48:05 +0200

    Date from custom field

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             foreach ($posts as $post) {
                 echo '<h1>'.$post->post_title.'</h1>';
                 echo '<p>'.$post->post_content.'</p>';
-                echo '<p>01/06/2017</p>';
+                echo '<p>'.get_post_custom_values('release_date', $post->ID)[0].'</p>';
             }
         ?>
     </article>

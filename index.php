c949c831b46f118fcb134d582a77d44fdae4dd2e
Rémy <remdub@live.fr>
2017-05-30 15:22:45 +0200

    add a name to newsletter form and a confirmation when mail successfully sent

--- index.php
+++ index.php
@@ -16,15 +16,23 @@
     <section>
         <h4>Soyez averti dès la sortie :</h4>
         <form method="post">
+            <input type="text" name="name" placeholder="votre nom">
             <input type="mail" name="mail" placeholder="Votre email">
             <button type="submit">Ok</button>
         </form>
         <?php
-            if(isset($_POST['mail'])){
-                $_POST['mail'];
-                $wpdb->insert( 'mails', array(
+            if(isset($_POST['mail']) && isset($_POST['name'])) {
+
+                $query = $wpdb->insert( 'mails', array(
                     'mail' => $_POST['mail'],
+                    'name' => $_POST['name'],
                 ));
+                if ($query) {
+                    echo "<p>Thank you, the mail " . $_POST['mail'] . " has been registered</p>";
+                } else {
+                    echo "<p>An error has occured, please try again</p>";
+                }
+
             }
         ?>
     </section>
